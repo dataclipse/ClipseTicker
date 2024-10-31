18f332f804af28c7bb3e1a6553b527b3acb2871b
rhiltner <ryan.hiltner@gmail.com>
2024-10-31 08:00:40 -0500

    Updates
    - Added Delete Job Functionality

--- frontend/src/scenes/jobs/index.jsx
+++ frontend/src/scenes/jobs/index.jsx
@@ -1,10 +1,11 @@
-import { Box, Button, Typography, useTheme } from "@mui/material";
-import { DataGrid, GridToolbarQuickFilter } from "@mui/x-data-grid";
+import { Box, Button, Typography, useTheme, Stack } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import React, { useState, useEffect } from "react";
 import Header from "../../components/header";
 import AddJobModal from "../../components/add_job_modal"
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { QuickSearchToolbar, formatRunTime } from "../../components/helper";
 
 const Jobs = () => {
@@ -13,43 +14,80 @@ const Jobs = () => {
     const [Jobs, setJobs] = useState([]);
     const [loading, setLoading] = useState(true);
     const [openJobModal, setOpenJobModal] = useState(false);
+    const handleOpenJobModal = () => setOpenJobModal(true);
+    const handleCloseJobModal = () => setOpenJobModal(false);
 
     const columns = [
         { field: "job_name", renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'Job Name'}</Typography>, flex: 1.5, },
         { field: "scheduled_start_time", renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'Scheduled Start Time'}</Typography>, flex: 1, },
-        { field: "status", renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'Status'}</Typography>, flex: 1, },
+        { field: "status", renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'Status'}</Typography>, flex: .6, },
         { field: "start_time", renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'Start Time'}</Typography>, flex: 1, },
         { field: "end_time", renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'End Time'}</Typography>, flex: 1, },
-        { field: "run_time", renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'Run Time'}</Typography>, flex: 1, },
+        { field: "run_time", renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'Run Time'}</Typography>, flex: .5, },
+        {
+            field: "action",
+            renderHeader: () => <Typography sx={{ fontWeight: 'bold' }}>{'Actions'}</Typography>,
+            renderCell: (params) => (
+                <Stack direction="row" spacing={1} alignItems={'center'} height={'100%'}>
+                    <DeleteIcon
+                        onClick={() => handleDeleteJob(params.row.job_name, params.row.scheduled_start_time)}
+                        sx={{ cursor: 'pointer', color: colors.redAccent[500], alignItems: 'center' }}
+                    />
+                </Stack>
+            ),
+            flex: .5,
+        },
     ];
 
-    useEffect(() => {
-        const fetchData = async () => {
+    const handleDeleteJob = async (job_name, scheduled_start_time) => {
+        if (window.confirm(`Are you sure you want to delete the job: ${job_name}?`)) {
             try {
-                const response = await fetch("/api/jobs");
-                const data = await response.json();
-                const formattedData = data.map((jobs, index) => ({
-                    id: index, 
-                    job_name: jobs.job_name,
-                    scheduled_start_time: jobs.scheduled_start_time,
-                    status: jobs.status,
-                    start_time: jobs.start_time,
-                    end_time: jobs.end_time,
-                    run_time: formatRunTime(jobs.run_time),
-                }));
-                setJobs(formattedData);
+                const response = await fetch(`/api/jobs`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ job_name, scheduled_start_time }),
+                });
+
+                if (response.ok) {
+                    // Refresh job data after deletion
+                    fetchData();
+                } else {
+                    console.error("Error deleting job:", response.statusText);
+                }
             } catch (error) {
-                console.error("Error fetching Jobs data:", error);
-            } finally {
-                setLoading(false);
+                console.error("Error deleting job:", error);
             }
-        };
+        }
+    };
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch("/api/jobs");
+            const data = await response.json();
+            const formattedData = data.map((jobs, index) => ({
+                id: index, 
+                job_name: jobs.job_name,
+                scheduled_start_time: jobs.scheduled_start_time,
+                status: jobs.status,
+                start_time: jobs.start_time,
+                end_time: jobs.end_time,
+                run_time: formatRunTime(jobs.run_time),
+            }));
+            setJobs(formattedData);
+        } catch (error) {
+            console.error("Error fetching Jobs data:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
         fetchData();
     }, []);
 
-    const handleOpenJobModal = () => setOpenJobModal(true);
-    const handleCloseJobModal = () => setOpenJobModal(false);
-    const handleSubmitJobModal = () => {handleCloseJobModal();};
+
 
     return (
         <Box m="20px">
@@ -65,7 +103,7 @@ const Jobs = () => {
             <AddJobModal
                 open={openJobModal}
                 onClose={handleCloseJobModal}
-                onSubmit={handleSubmitJobModal}
+                onSubmit={handleCloseJobModal}
             />
 
             <Box
